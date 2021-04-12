ea05f9f76567bc6ae61237837402cccf1e3c03c4
Banji Ajayi <olabanji.ajayi@gmail.com>
2021-04-12 15:04:07 +0100

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed = 100f;
+    [SerializeField] float rotationSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(Vector3.up * speed * Time.deltaTime);
+            Debug.Log("Up");
+        }
+
+
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+            Debug.Log("right");
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+            Debug.Log("left");
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+}
